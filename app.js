490e02d55b6a2c0e209ddfcd30995cc8bea6409f
Hubert <hub28yp@gmail.com>
2019-11-22 15:09:43 +0100

    ajout embed ip

--- app.js
+++ app.js
@@ -2,15 +2,62 @@ const Eris = require("eris");
 var prefix = "!";
 var bot = new Eris("NjQ3MzgzMjgyOTQ1NzUzMDkx.Xde5CA.atvajS5fxYMu9M4xZnA1ipbbpuI");
 bot.on("ready", () => {
-    console.log("Ready!");
+    console.log("Je suis prêt !");
 });
 bot.on("messageCreate", (msg) => {
     switch(msg.content){
-        case prefix + "ping":
-                
-        bot.createMessage(msg.channel.id, "Envois ton 06 !");
-
-            break;
+        case prefix + "ip":
+                const data = {
+                    "content": "this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```",
+                    "embed": {
+                      "title": "title ~~(did you know you can have markdown here too?)~~",
+                      "description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
+                      "url": "https://discordapp.com",
+                      "color": 11676803,
+                      "timestamp": "2019-11-22T14:08:48.582Z",
+                      "footer": {
+                        "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+                        "text": "footer text"
+                      },
+                      "thumbnail": {
+                        "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+                      },
+                      "image": {
+                        "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+                      },
+                      "author": {
+                        "name": "author name",
+                        "url": "https://discordapp.com",
+                        "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+                      },
+                      "fields": [
+                        {
+                          "name": "🤔",
+                          "value": "some of these properties have certain limits..."
+                        },
+                        {
+                          "name": "😱",
+                          "value": "try exceeding some of them!"
+                        },
+                        {
+                          "name": "🙄",
+                          "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+                        },
+                        {
+                          "name": "<:thonkang:219069250692841473>",
+                          "value": "these last two",
+                          "inline": true
+                        },
+                        {
+                          "name": "<:thonkang:219069250692841473>",
+                          "value": "are inline fields",
+                          "inline": true
+                        }
+                      ]
+                    }
+                  };
+                  client.createMessage(channelID, data);
+        break;
     }
     if(msg.content === "!ip") {
         bot.createMessage(msg.channel.id, "Tu dois effectuer un ip check via ton panel joueur !");
---
+++ test.js
@@ -0,0 +1,49 @@
+{
+    "content": "this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```",
+    "embed": {
+      "title": "_Salut, je suis BOT HUB et j'ai la réponse à ta question ! 🙂_ ",
+      "description": "Je suis utilisé pour t'aider dans tes demandes de support !  ```\nyes, even code blocks```",
+      "url": "https://discordapp.com",
+      "color": 4281280,
+      "footer": {
+        "icon_url": "https://yt3.ggpht.com/a/AGF-l79CpWCFUqmlrCeWtYdy6l4MqJvWllNEDsbtPg=s900-c-k-c0xffffffff-no-rj-mo",
+        "text": "by BOT HUB"
+      },
+      "thumbnail": {
+        "url": "https://image.noelshack.com/fichiers/2019/47/5/1574430989-67600674-521441015329765-5369664045848723456-n.png"
+      },
+      "image": {
+        "url": "https://nicolasmariuzza.com/assets/splash_1plan-de-travail-8.jpg "
+      },
+      "author": {
+        "name": "BOT HUB",
+        "url": "https://www.adastragaming.fr",
+        "icon_url": "https://pbs.twimg.com/profile_images/1111412977347919872/t2Fkyw_t_400x400.png"
+      },
+      "fields": [
+        {
+          "name": "Tu n'arrives pas à te connecter au serveur ? 🤔",
+          "value": "Pense à effectuer un IP check via ton panel de joueur !"
+          "value": "Bon je suis gentil .. je te donne le [lien](https://account.gtav.adastragaming.fr/login)"
+        },
+        {
+          "name": "😱",
+          "value": "try exceeding some of them!"
+        },
+        {
+          "name": "🙄",
+          "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+        },
+        {
+          "name": "<:thonkang:219069250692841473>",
+          "value": "these last two",
+          "inline": true
+        },
+        {
+          "name": "<:thonkang:219069250692841473>",
+          "value": "are inline fields",
+          "inline": true
+        }
+      ]
+    }
+  }
