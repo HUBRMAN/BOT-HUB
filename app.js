a352acbb19ce471c01dfbb27459ccde8070a34e9
Hubert <hub28yp@gmail.com>
2019-11-22 18:24:29 +0100

    Rajout deuxieme commande

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Eris = require("eris");
-const Embed_Builder = require("eris_embed_builder");
 const prefix = "!";
-var bot = new Eris("NjQ3MzgzMjgyOTQ1NzUzMDkx.XdgHSg.FcER91V6sm5hdIPMHcEZcsTUPqE");
+var bot = new Eris("NjQ3MzgzMjgyOTQ1NzUzMDkx.XdgIPA.vEezag6Mf70vPqbNZNnwVr5FLMU");
 bot.on("ready", () => {
     console.log("Je suis prêt et opérationnel !");
 });
@@ -11,18 +10,18 @@ bot.on("messageCreate", (msg) => {
                 const data = {
                     "embed": {
                       "title": "Problème de connexion au serveur !",
-                      "description": "Je suis BOT HUB, et je suis là pour t'aider à résoudre ton problème 😃 \n \n \n",
+                      "description": "Je suis le BOT support d'Adastra et je suis là pour t'aider à résoudre ton problème 😃 \n \n \n",
                       "url": "https://discordapp.com",
                       "color": 4886754,
                       "footer": {
                         "icon_url": "https://yt3.ggpht.com/a/AGF-l79CpWCFUqmlrCeWtYdy6l4MqJvWllNEDsbtPg=s900-c-k-c0xffffffff-no-rj-mo",
-                        "text": "copyright AdastraGaming"
+                        "text": "by AdastraGaming"
                       },
                       "thumbnail": {
                         "url": "https://image.noelshack.com/fichiers/2019/47/5/1574430989-67600674-521441015329765-5369664045848723456-n.png"
                       },
                       "image": {
-                        "url": "https://nicolasmariuzza.com/assets/splash_1plan-de-travail-8.jpg"
+                        "url": "https://image.noelshack.com/fichiers/2019/47/5/1574439735-annotation-2019-11-22-172144.png"
                       },
                       "fields": [
                         {
@@ -31,21 +30,47 @@ bot.on("messageCreate", (msg) => {
                         },
                         {
                           "name": "Tu as déjà fait un IP check sur le pannel ? 😱",
-                          "value": "N'oublies pas que nous avons changé de MP, \nnous sommes maintenant sur [ALTV](https://altv.mp/#/) !\nPour te connecter c'est très simple, voici l'adresse \n ```connect.adastragaming.fr:80```"
+                          "value": "\nN'oublies pas que nous avons changé de MP, \nnous sommes maintenant sur [ALTV](https://altv.mp/#/) !\nPour te connecter c'est très simple, voici l'adresse \n ```connect.adastragaming.fr:80```"
                         },
                         {
                           "name": "🙄",
-                          "value": " **Si ton problème persiste, n'hésite pas à contacter un @support ! "
+                          "value": "Si ton problème persiste, n'hésite pas à contacter un @support ! "
                         },
                       ]
                     }
                   };
                   bot.createMessage(msg.channel.id, data);
         break;
-    }
-    if(msg.content === "!ip") {
-        bot.createMessage(msg.channel.id, "Tu dois effectuer un ip check via ton panel joueur !");
         
+        case prefix + "tfr":
+            const data1 = {
+              "embed": {
+                "title": "Ton plugin vocal se déconnecte toutes les 5 secondes ? 🤐",
+                "description": "\n \nHello je suis le BOT support d'Adastra et je suis là pour t'aider à résoudre ton problème ! ",
+                "color": 4886754,
+                "footer": {
+                  "icon_url": "https://yt3.ggpht.com/a/AGF-l79CpWCFUqmlrCeWtYdy6l4MqJvWllNEDsbtPg=s900-c-k-c0xffffffff-no-rj-mo",
+                  "text": "by AdastraGaming"
+                },
+                "thumbnail": {
+                  "url": "https://image.noelshack.com/fichiers/2019/47/5/1574430989-67600674-521441015329765-5369664045848723456-n.png"
+                },
+                "image": {
+                  "url": "https://image.noelshack.com/fichiers/2019/47/5/1574442204-screenshot-150.jpg"
+                },
+                "fields": [
+                  {
+                    "name": "\n \nAs-tu vérifié que tu n'as qu'un plugin TFR activé ? 🤔",
+                    "value": "\n \nLe TFR Adastra ne fonctionne pas si un autre TFR est activé .. "
+                  },
+                  {
+                    "name": "Tu n'as pas encore le TFR ?! 😱",
+                    "value": "Télécharger le [plugin](https://static.adastragaming.fr/gtav/plugin/adastra_tfr_latest.ts3_plugin) !"
+                  },
+                ]
+              }
+            };
+            bot.createMessage(msg.channel.id, data1  );
     }
 });
 bot.connect();
